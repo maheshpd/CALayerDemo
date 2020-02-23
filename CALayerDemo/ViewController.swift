ba28f74ca2915407fd38c6d98a6e6fa204c073a0
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-02-23 15:30:25 +0530

    initial commit

--- CALayerDemo/ViewController.swift
+++ CALayerDemo/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var practiceView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        practiceView.layer.backgroundColor = UIColor.blue.cgColor
+        practiceView.layer.cornerRadius = CGFloat(10.0)
+        practiceView.layer.borderWidth = CGFloat(5.0)
+        practiceView.layer.borderColor = UIColor.orange.cgColor
+        practiceView.layer.shadowOpacity = 0.7
+        practiceView.layer.shadowRadius = CGFloat(15.0)
+        practiceView.layer.shadowOffset = CGSize(width: CGFloat(-15), height: CGFloat(15))
+        practiceView.layer.contents = UIImage(named: "p20")?.cgImage
+        practiceView.layer.masksToBounds = true
     }
 
 
